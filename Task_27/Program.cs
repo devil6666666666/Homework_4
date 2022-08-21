4c6ac1474f3622aae50e754d96047505449e64a3
Viktor A <abcd999@bk.ru>
2022-08-22 00:32:39 +0300

    Задача 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает на вход число и выдает сумму цифр в этом числе.
+// Если введено отрицательное число, минус относится к первой цифре.
+
+Console.WriteLine("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (Math.Abs(N) > 10)
+{
+    sum = sum + Math.Abs(N % 10);
+    N = N / 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {sum + N}");
